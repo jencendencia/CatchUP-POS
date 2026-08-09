5b0c89b5e0c804816cc950131ab57c368ae636e3
ictlinksolutions2017 <ictlinksolutions2017@gmail.com>
2026-08-09 15:57:40 +0800

    chore: Bump version to 1.3.0 (10300)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.catchuppos.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10202
-        versionName = "1.2.2"
+        versionCode = 10300
+        versionName = "1.3.0"
 
         vectorDrawables {
             useSupportLibrary = true
